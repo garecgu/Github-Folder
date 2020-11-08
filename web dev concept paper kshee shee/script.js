f7c5b6bf6fec169a6a2c38b112e1c98abc51f365
garecgu <41191586+garecgu@users.noreply.github.com>
2020-11-08 14:30:46 +0800

    yes 2 30 update

--- web dev concept paper kshee shee/script.js
+++ web dev concept paper kshee shee/script.js
@@ -1,18 +1,33 @@
-let tabHead = document.getElementsByClassName("tab-head")[0];
-let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
-let tabBody = document.getElementsByClassName("tab-body")[0];
+let tabHead = document.getElementsByClassName(tab-head)[0];
+let tabIndicator = document.getElementsByClassName(tab-indicator)[0];
+let tabBody = document.getElementsByClassName(tab-body)[0];
 
-let tabsPane = tabHead.getElementsByTagName("div");
+let tabsPane = tabHead.getElementsByTagName(div);
 
-for( let i=0; i<tabsPane.length; i++ )
+for( let i=0; itabsPane.length; i++ )
 {
-    tabsPane[i].addEventListener("click", function()
+    tabsPane[i].addEventListener(click, function()
     {
-        tabHead.getElementsByClassName("sets")[0].classList.remove("sets");
-        tabsPane[i].classList.add("sets");
-        tabBody.getElementsByClassName("sets")[0].classList.remove("sets");
-        tabBody.getElementsByTagName("div")[i].classList.add("sets");
+        tabHead.getElementsByClassName(sets)[0].classList.remove(sets);
+        tabsPane[i].classList.add(sets);
+        tabBody.getElementsByClassName(sets)[0].classList.remove(sets);
+        tabBody.getElementsByTagName(div)[i].classList.add(sets);
 
-        tabIndicator.style.left = `calc(calc(100% / 2) * ${i})`;
+        tabIndicator.style.left = `calc(calc(100%  2)  ${i})`;
     });
+}
+
+var coll = document.getElementsByClassName(collapsible);
+var i;
+
+for (i = 0; i  coll.length; i++) {
+  coll[i].addEventListener(click, function() {
+    this.classList.toggle(active);
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + px;
+    } 
+  });
 }
